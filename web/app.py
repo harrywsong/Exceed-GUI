fdad82cd45c7b04e42b5e579df7524ee4a5b88ae
Woohyuk Song <harrywsong@naver.com>
2025-07-14 01:03:15 -0400

    1

--- web/app.py
+++ web/app.py
@@ -315,7 +315,7 @@ def run_flask_app():
     """
     Starts the Flask web server for the UI.
     """
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 if __name__ == '__main__':
     # The bot's API server is assumed to be started by bot.py itself.
